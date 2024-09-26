f30336352c1735f38b2fccac0328380466d11f8a
ganangAbimanyu <lilmanyuuu@gmail.com>
2024-09-26 16:58:45 +0800

    version 1.2

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
     // Add smooth scrolling to all links
     $("a").on('click', function(event) {
@@ -12,19 +11,19 @@ $(document).ready(function(){
         var hash = this.hash;
   
         // Using jQuery's animate() method to add smooth page scroll
-        $('html, body').animate({.
+        $('html, body').animate({
           scrollTop: $(hash).offset().top
         }, 800, function(){
      
           // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
         });
-      }
-    });
-  });
+      }
+    });
+  });
 
 
-  // Function to detect if the user is on a mobile device
+// Function to detect if the user is on a mobile device
 // Function to detect if the user is on a mobile device
 function isMobile() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
